b73b8ef3dd239cb03eae248302efd645c364e379
Hiroshi Kimura <hiroshi.kimura.0331m@gmail.com>
2025-04-25 20:51:42 +0900

    wrongly tagged. correct.

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -11,7 +11,7 @@
    [typing-ex.plot :refer [scatter]]
    #_[clojure.test :as t]))
 
-(def ^:private version "0.42.7")
+(def ^:private version "4.32.8")
 
 ;--------------------------------
 (defn- ss
