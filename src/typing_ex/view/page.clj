9318983172576ccf35e0fde984c88ffde90cde33
Hiroshi Kimura <hiroshi.kimura.0331m@gmail.com>
2025-05-09 21:48:24 +0900

    30x3

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -11,7 +11,7 @@
    [typing-ex.plot :refer [scatter]]
    #_[clojure.test :as t]))
 
-(def ^:private version "4.33.1")
+(def ^:private version "4.33.2")
 
 ;--------------------------------
 (defn- ss
