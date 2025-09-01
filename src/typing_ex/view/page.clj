833e93243f6d59c39f057500f5f173d46cac3617
Hiroshi Kimura <hkim@hkim.jp>
2025-09-01 09:32:18 +0900

    restarted on app.melt

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -8,7 +8,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [typing-ex.plot :refer [scatter]]))
 
-(def ^:private version "4.45.1249")
+(def ^:private version "0.46.0")
 
 ;--------------------------------
 (defn- ss
