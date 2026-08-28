d528b986e7959ecc5721171386f7dee91733d950
Hiroshi Kimura <hkim@hkim.jp>
2026-08-28 13:50:33 +0900

    bump version

--- src/typing_ex/view/page.clj
+++ src/typing_ex/view/page.clj
@@ -9,7 +9,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [typing-ex.plot :refer [scatter]]))
 
-(def ^:private version "6.0.1478")
+(def ^:private version "6.0.1481")
 
 ;--------------------------------
 (defn- ss
