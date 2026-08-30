9663af8e397f2279479a5a1ab702580f7f634877
hkimjp <hkim@hkim.jp>
2026-08-30 12:12:57 +0900

    deleted src/typing_ex/totals.clj

--- src/typing_ex/totals.clj
+++
@@ -1,9 +0,0 @@
-(ns typing-ex.totals
-  (:require
-   [typing-ex.boundary.roll-calls :as roll-calls]
-   [typing-ex.boundary.results :as results]))
-
-(defn attend [login]
-  (roll-calls/attend login))
-
-(attend "hkimura")
