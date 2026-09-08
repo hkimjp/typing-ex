489352006cf32ab44553b244274d52258b8cd76a
hkimjp <hkim@hkim.jp>
2026-09-08 13:50:06 +0900

    enbug...

--- src/typing_ex/typing.cljs
+++ src/typing_ex/typing.cljs
@@ -15,8 +15,8 @@
 (def ^:private todays-limit 10)
 
 ;; bump-version.sh will rewrite
-(def ^:private timeout
-  (int (.-value (.getElementById js/document "timeout"))))
+(def ^:private timeout 60
+  #_(int (.-value (.getElementById js/document "timeout"))))
 
 (def interval (atom 1000)) ;; milli second
 
